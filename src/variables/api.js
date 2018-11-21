e6ae52ffd90ce86bab2edd03f32f6c90895d6f1b
ambat25 <25ambat25@gmail.com>
2018-11-21 13:56:40 +0100

    added results url

--- src/variables/api.js
+++ src/variables/api.js
@@ -4,3 +4,4 @@ export const ALL_PATIENTS_URL = `${API_URL}/api/patients`;
 export const PATIENT_URL = `${API_URL}/api/patient`;
 export const LAB_TEST_URL = `${API_URL}/api/tests`;
 export const ALL_DOCTORS_URL = `${API_URL}/api/doctors`;
+export const RESULTS_URL = `${API_URL}/api/results`;
