35855c3c60c4c0acbaa441608408d0e4bf403fda
ambat25 <25ambat25@gmail.com>
2018-11-21 14:00:39 +0100

    added result reducer to store

--- src/store.js
+++ src/store.js
@@ -2,9 +2,10 @@ import patientReducer from './reducers/patients';
 import doctorReducer from './reducers/doctors';
 import notificationReducer from './reducers/notifications';
 import testReducer from './reducers/lab_test';
+import resultReducer from './reducers/results';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({ patientReducer, doctorReducer, notificationReducer, testReducer });
+const reducer = combineReducers({ patientReducer, doctorReducer, notificationReducer, testReducer, resultReducer });
 export default createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
