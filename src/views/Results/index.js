7f7f06c5489dbc5b071c972c8bf0e4e1833d2bba
ambat25 <25ambat25@gmail.com>
2018-11-21 14:00:01 +0100

    made result view

--- src/views/Results/index.js
+++ src/views/Results/index.js
@@ -1,118 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Grid, Row, Col, Form, FormGroup, ControlLabel, Table, FormControl, Modal, Button } from 'react-bootstrap';
 
-class UserProfile extends Component {
-	state = {
-		showModal: false
-	};
-	closeModal = () => {
-		this.setState({
-			showModal: false
-		});
-	};
-	savePatient = () => {
-		this.setState({
-			showModal: false
-		});
-	};
-	openModal = () => {
-		this.setState({
-			showModal: true
-		});
-	};
+class Results extends Component {
 	render() {
 		return (
 			<div className="content">
 				<Grid>
-					<div style={{ textAlign: 'right', marginBottom: '10px' }}>
-						<Button className="btn-circle btn-xl" onClick={this.openModal}>
-							<i className="fa fa-plus" />
-						</Button>
-					</div>
 					<Table striped bordered condensed hover>
 						<thead>
 							<tr>
 								<th>#</th>
-								<th>First Name</th>
-								<th>Last Name</th>
-								<th>Username</th>
+								<th>Test Id</th>
+								<th>Patient</th>
+								<th>Doctor</th>
+								<th>Tests</th>
+								<th>Date</th>
 							</tr>
 						</thead>
 						<tbody>
-							<tr>
-								<td>1</td>
-								<td>Mark</td>
-								<td>Otto</td>
-								<td>@mdo</td>
-							</tr>
+							{this.props.results.map((result, index) => (
+								<tr key={result._id}>
+									<td>{index + 1}</td>
+									<td>{result._id}</td>
+									<td>{result.patient.name}</td>
+									<td>{result.doctor.name}</td>
+									<td>{result.tests.map((test) => test.name).join(', ')}</td>
+									<td>{new Date(result._id).toDateString()}</td>
+								</tr>
+							))}
 						</tbody>
 					</Table>
 				</Grid>
-				<div className="static-modal">
-					<Modal
-						show={this.state.showModal}
-						container={this}
-						onHide={this.closeModal}
-						aria-labelledby="contained-modal-title"
-					>
-						<Modal.Header closeButton>
-							<Modal.Title id="contained-modal-title">New Patient</Modal.Title>
-						</Modal.Header>
-						<Modal.Body>
-							<Grid fluid>
-								<Form>
-									<Row>
-										<Col sm={6}>
-											<FormGroup>
-												<ControlLabel> Name </ControlLabel>
-												<FormControl placeholder="Aliyu Ilyasu" />
-											</FormGroup>
-										</Col>
-										<Col sm={6}>
-											<FormGroup>
-												<ControlLabel> Phone </ControlLabel>
-												<FormControl type="phone" required={true} placeholder="080xxxxxxxx" />
-											</FormGroup>
-										</Col>
-										<Col sm={6}>
-											<FormGroup>
-												<ControlLabel> Age </ControlLabel>
-												<FormControl type="number" required={true} placeholder="15" min={1} />
-											</FormGroup>
-										</Col>
-										<Col sm={6}>
-											<FormGroup controlId="formControlsSelect">
-												<ControlLabel>Gender</ControlLabel>
-												<FormControl componentClass="select" placeholder="select">
-													<option value="">select</option>
-													<option value="male">Male</option>
-													<option value="female">female</option>
-												</FormControl>
-											</FormGroup>
-										</Col>
-										<Col sm={6}>
-											<FormGroup controlId="formControlsTextarea">
-												<ControlLabel>Address</ControlLabel>
-												<FormControl
-													componentClass="textarea"
-													placeholder="Address"
-													style={{ height: 100 }}
-												/>
-											</FormGroup>
-										</Col>
-									</Row>
-								</Form>
-							</Grid>
-						</Modal.Body>
-						<Modal.Footer>
-							<Button onClick={this.savePatient}>Save</Button>
-							<Button onClick={this.closeModal}>Close</Button>
-						</Modal.Footer>
-					</Modal>
-				</div>
 			</div>
 		);
 	}
 }
 
-export default UserProfile;
+export default connect((state) => ({
+	results: state.resultReducer.results
+}))(Results);
